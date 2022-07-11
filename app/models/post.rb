03bd26824ef96e922a19ef2298af429261cb5734
Takashi <th2826@columbia.edu>
2022-07-11 12:32:13 -0400

    compledted assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  validate :must_include_these_words
+
+  def must_include_these_words
+    if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      errors.add(:title, 'Your title is not clickbait-y')
+    end
+  end
 end
